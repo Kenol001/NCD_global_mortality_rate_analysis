afff4b7ab82ad5dd770f6f7af6ee981187cb2fcf
Purity K <puritykoima2020@gmail.com>
2026-07-11 04:03:03 +0300

    Update 3.1descriptive_statistics.R

--- d_r_scripts/3.1descriptive_statistics.R
+++ d_r_scripts/3.1descriptive_statistics.R
@@ -1,1 +1,15 @@
 summary(NCD_global_mortality_rate$Value)
+ggplot(NCD_global_mortality_rate,
+       aes(x = ParentLocation,
+           y = `Mortality Rate`,
+           fill = ParentLocation)) +
+  geom_boxplot() +
+  labs(
+    title = "Distribution of NCD Mortality Rate by WHO Region",
+    x = "WHO Region",
+    y = "Mortality Rate (per 100,000 population)"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    plot.title = element_text(face = "bold", hjust = 0.5)
